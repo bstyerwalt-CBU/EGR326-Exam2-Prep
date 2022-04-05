4ad50237eda9e363d44a3056e3d7210409798ea5
bstyerwalt@calbaptist.edu <bstyerwalt@calbaptist.edu>
2022-04-04 18:23:07 -0700

    removing rec from point

--- src/flyweight/Point.java
+++ src/flyweight/Point.java
@@ -6,24 +6,22 @@ import java.util.Map;
 public class Point {
     private final int x;
     private final int y;
-    private final Rectangle rec;
     private volatile static Map<String, Point> map;
 
-    public Point getInstance(int x, int y, Rectangle rec) {
-        String key = x + "," + y + "," + "," + rec;
+    public Point getInstance(int x, int y) {
+        String key = x + "," + y;
         if (!map.containsKey(key)) {
             synchronized (Point.class) {
                 if (!map.containsKey(key)) {
-                    map.put(key, new Point(x, y, rec));
+                    map.put(key, new Point(x, y));
                 }
             }
         }
         return map.get(key);
     }
 
-    private Point(int x, int y, Rectangle rec) {
+    private Point(int x, int y) {
         this.x = x;
         this.y = y;
-        this.rec = rec;
     }
 }
